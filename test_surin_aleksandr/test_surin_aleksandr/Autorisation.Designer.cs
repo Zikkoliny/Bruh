6d7b87083fa29c81866125885d60aa196d0f786d
b302-08 <b302-08@gmail.com>
2019-12-17 11:58:02 +0300

    gdsagsad

--- test_surin_aleksandr/test_surin_aleksandr/Autorisation.Designer.cs
+++ test_surin_aleksandr/test_surin_aleksandr/Autorisation.Designer.cs
@@ -39,18 +39,18 @@
             // 
             // loginBox
             // 
-            this.loginBox.Location = new System.Drawing.Point(207, 152);
+            this.loginBox.Location = new System.Drawing.Point(181, 105);
             this.loginBox.Name = "loginBox";
-            this.loginBox.Size = new System.Drawing.Size(100, 20);
+            this.loginBox.Size = new System.Drawing.Size(155, 20);
             this.loginBox.TabIndex = 0;
             this.loginBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.loginBox.TextChanged += new System.EventHandler(this.loginBox_TextChanged);
             // 
             // passBox
             // 
-            this.passBox.Location = new System.Drawing.Point(207, 259);
+            this.passBox.Location = new System.Drawing.Point(181, 209);
             this.passBox.Name = "passBox";
-            this.passBox.Size = new System.Drawing.Size(100, 20);
+            this.passBox.Size = new System.Drawing.Size(155, 20);
             this.passBox.TabIndex = 1;
             this.passBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -58,7 +58,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(231, 126);
+            this.label1.Location = new System.Drawing.Point(230, 71);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(52, 16);
             this.label1.TabIndex = 2;
@@ -68,7 +68,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(227, 232);
+            this.label2.Location = new System.Drawing.Point(226, 177);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(64, 16);
             this.label2.TabIndex = 3;
@@ -78,7 +78,7 @@
             // 
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.button2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(179, 399);
+            this.button2.Location = new System.Drawing.Point(181, 337);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(149, 39);
             this.button2.TabIndex = 5;
@@ -103,7 +103,7 @@
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.button1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(209, 354);
+            this.button1.Location = new System.Drawing.Point(211, 292);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(89, 39);
             this.button1.TabIndex = 4;
@@ -116,7 +116,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(213)))), ((int)(((byte)(202)))));
-            this.ClientSize = new System.Drawing.Size(522, 450);
+            this.ClientSize = new System.Drawing.Size(522, 391);
             this.Controls.Add(this.Label3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
